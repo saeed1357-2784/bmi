dd37671ff0d1e4d4d982f18c61c9c6f962fd74cc
Ehsan <ehsanyfard@gmail.com>
2020-03-24 14:32:30 +0430

    first commit

---
+++ bmiscript.js
@@ -0,0 +1,11 @@
+
+
+function bmiyou() {
+    var x = document.getElementById("vazn1").value;
+    var y = document.getElementById("ghad1").value;
+    var w = y * y;
+    var z = x / w;
+    document.getElementById("bmiu1").innerHTML+="</br>"
+}
+
+
