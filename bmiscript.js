86db5b3f400b9ec67af1cb11275b22542e2e3908
Hamid <tuytoosh@yahoo.com>
2020-03-27 13:59:08 +0430

    add event listener to btn

--- bmiscript.js
+++ bmiscript.js
@@ -1,6 +1,7 @@
 
 
-function bmiyou() {
+function bmiyou(e) {
+    e.preventDefault();
     var x = document.getElementById("vazn1").value;
     var y = document.getElementById("ghad1").value;
     var w = y * y;
@@ -8,4 +9,23 @@ function bmiyou() {
     document.getElementById("bmiu1").innerHTML+="</br>"
 }
 
+
+submit2 = document.querySelector('#submit2');
+
+submit2.addEventListener('click', calc);
+
+function calc(e) {
+    e.preventDefault();
+    var x = document.getElementById("vazn1").value;
+    var y = document.getElementById("ghad1").value;
+
+    console.log(x, y)
+
+    var w = y * y;
+    var z = x / w;
+
+    var node = document.createElement('p');
+    node.textContent = z;
+    document.getElementById("bmiu1").appendChild(node);
+}
 
